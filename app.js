9df8054b5f945397286aa0b3811b34943020254a
Jef King <jefkin@microsoft.com>
2016-10-20 16:17:11 -0600

    misc

---
+++ app.js
@@ -0,0 +1,111 @@
+﻿var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var OAuth = require('oauth').OAuth;
+
+var oa = new OAuth(
+    "https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"q2JfZwE8BTIHHNruzRqaWubEI",
+	"FI4nM1BuayKsMBLZ2fA4zicaOkNUvjrhfRgiDYngVQVBtkXRyT",
+	"1.0",
+	"http://localhost:1337/auth/twitter/callback",
+	"HMAC-SHA1"
+);
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/auth/twitter', function (req, res) {
+    oa.getOAuthRequestToken(function (error, oauth_token, oauth_token_secret, results) {
+        if (error) {
+            console.log(error);
+            res.send("yeah no. didn't work.")
+        }
+        else {
+            req.session.oauth = {};
+            req.session.oauth.token = oauth_token;
+            console.log('oauth.token: ' + req.session.oauth.token);
+            req.session.oauth.token_secret = oauth_token_secret;
+            console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+            res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token)
+        }
+    });
+});
+app.get('/auth/twitter/callback', function (req, res, next) {
+    if (req.session.oauth) {
+        req.session.oauth.verifier = req.query.oauth_verifier;
+        var oauth = req.session.oauth;
+        
+        oa.getOAuthAccessToken(oauth.token, oauth.token_secret, oauth.verifier, 
+		function (error, oauth_access_token, oauth_access_token_secret, results) {
+            if (error) {
+                console.log(error);
+                res.send("yeah something broke.");
+            } else {
+                req.session.oauth.access_token = oauth_access_token;
+                req.session.oauth, access_token_secret = oauth_access_token_secret;
+                console.log(results);
+                res.send("worked. nice one.");
+            }
+        }
+        );
+    } else
+        next(new Error("you're not supposed to be here."))
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
