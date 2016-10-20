a7843f4c4596b6fc5b0d65da7bb550624a2230f4
Jef King <jefkin@microsoft.com>
2016-10-20 17:16:07 -0600

    move location

--- server.js
+++ server.js
@@ -49,15 +49,13 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+// Use application-level middleware for common functionality, including logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+// Initialize Passport and restore authentication state, if any, from the session.
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -65,11 +63,6 @@ app.use(passport.session());
 app.get('/',
   function (req, res) {
     res.render('home', { user: req.user });
-  });
-
-app.get('/login',
-  function (req, res) {
-    res.render('login');
   });
 
 app.get('/login/twitter', passport.authenticate('twitter'));
