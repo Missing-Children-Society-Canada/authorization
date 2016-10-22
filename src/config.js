08a8c27b2fd159962f3a858965f53ab0a5bca3a4
Jef King <jefkin@microsoft.com>
2016-10-22 11:28:08 -0600

    config change for dev

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-
+//DocumentDB: Profile Storage
 config.host = "https://testingnodeprofile.documents.azure.com:443/";
 config.authKey = process.env.AUTH_KEY || "5dX4ZXzSUPn1VuHP59K1IQGDEJbiUojGZuHc6HrivRSWIxs5lZrtAYnEidoZi4E7jlPTBP4kcFoiRzn4tLZAwQ====";
 config.databaseId = "missingdata";
@@ -12,6 +12,6 @@ config.collLink = 'dbs/' + config.databaseId + '/colls/' + config.collectionId;
 //Twitter configration
 config.consumerKey = 'q2JfZwE8BTIHHNruzRqaWubEI';
 config.consumerSecret = 'FI4nM1BuayKsMBLZ2fA4zicaOkNUvjrhfRgiDYngVQVBtkXRyT';
-config.callbackURL = 'http://localhost:8080/login/twitter/return';
+config.callbackURL = 'http://mcsc-authorization-dev.azurewebsites.net/login/twitter/return';
 
 module.exports = config;
