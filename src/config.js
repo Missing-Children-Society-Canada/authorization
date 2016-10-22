8e56af8b4b39b7e236c0c670910f629e5b8f86fb
Jef King <jefkin@microsoft.com>
2016-10-22 14:29:21 -0600

    bad config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,15 @@
 var config = {}
 
 //DocumentDB: Profile Storage
+config.host = process.env.DOC_DB || 'https://XXXXXXXXXXXXXXXXXXXXXXXX.documents.azure.com:443/';
+config.authKey = process.env.AUTH_KEY || 'XXXXXXXXXXXXXXXXXXXXXXXX==';
 config.databaseId = "missingdata";
 config.FacebookCollectionId = "facebook-profile";
 config.TwitterCollectionId = "twitter-profile";
 
 //Twitter configration
+config.consumerKey = process.env.TWITTER_CONSUMER_KEY || 'XXXXXXXXXXXXXXXXXXXXXXXX';
+config.consumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'XXXXXXXXXXXXXXXXXXXXXXXX';
 config.callbackURL = process.env.ROOT + '/login/twitter/return';
 
 //Telemitry
