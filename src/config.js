77d6a7d5e67abce9769b9ff33a0bde68432dd8f5
Jef King <jefkin@microsoft.com>
2016-10-22 18:07:36 -0600

    amature hour

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ config.FacebookCollectionId = "facebook-profile";
 config.TwitterCollectionId = "twitter-profile";
 
 //Twitter configration
-config.consumerKey = process.env.TWITTER_CONSUMER_KEY || 'XXXXXXXXXXXXXXXXXXXXXXXX';
-config.consumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'XXXXXXXXXXXXXXXXXXXXXXXX';
-config.callbackURL = process.env.ROOT + '/login/twitter/return';
+config.ConsumerKey = process.env.TWITTER_CONSUMER_KEY || 'XXXXXXXXXXXXXXXXXXXXXXXX';
+config.ConsumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'XXXXXXXXXXXXXXXXXXXXXXXX';
+config.CallbackURL = process.env.ROOT + '/login/twitter/return';
 
 //Telemitry
 config.appInsightsKey = process.env.APP_INSIGHTS_KEY || 'XXXXXXXXXXXXXXXXXXXXXXXX';
