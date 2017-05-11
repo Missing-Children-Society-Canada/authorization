1cd9a814351e2223299e94b369f9cd82c7b2aa80
Jef King <jefkin@microsoft.com>
2017-05-10 18:49:31 -0700

    misc

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 // load all the things we need
-var LocalStrategy = require('passport-local').Strategy;
+//var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
 var InstagramStrategy = require('passport-instagram').Strategy;
