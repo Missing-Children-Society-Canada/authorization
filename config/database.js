e8d05d84b09430e613223c6c696ead04d46c08cc
Jef King <jefkin@microsoft.com>
2017-05-10 18:09:08 -0700

    config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // config/database.js
 module.exports = {
-  'url': 'XXXXXXXXXXXXXXXXX'
+  'url': 'mongodb://missingchildrendata:agHqTdTA7xaIlSEnaVU63O0sFCnrSZuZHYcBLLoUgeyPCYC5qYSSFca7eMGbqRFmPaAPeaakJmMxYSy0OvxHog==@missingchildrendata.documents.azure.com:10250/mean-dev?ssl=true&sslverifycertificate=false'
 };
