42b468bd91d88f9a979261baabaadc3f1f2bad4d
Jef King <jefkin@microsoft.com>
2017-05-10 18:20:20 -0700

    fix

--- config/auth.js
+++ config/auth.js
@@ -4,20 +4,21 @@
 module.exports = {
 
 	'facebookAuth': {
-		'clientID': 'XXXXXXXXXXXXXXXXXXXXXXXXX', // your App ID
-		'clientSecret': 'XXXXXXXXXXXXXXXXXXXXXXXXX', // your App Secret
-		'callbackURL': 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+		'clientID': '189849514869492', // your App ID
+		'clientSecret': 'fce4a5d219289346239642928a1f7c79', // your App Secret
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'twitterAuth': {
-		'consumerKey': 'XXXXXXXXXXXXXXXXXXXXXXXXX',
-		'consumerSecret': 'XXXXXXXXXXXXXXXXXXXXXXXXX',
-		'callbackURL': 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+		'consumerKey': 'q2JfZwE8BTIHHNruzRqaWubEI',
+		'consumerSecret': 'FI4nM1BuayKsMBLZ2fA4zicaOkNUvjrhfRgiDYngVQVBtkXRyT',
+		'callbackURL': 'http://localhost:8080/auth/twitter/callback'
 	},
 
 	'instagramAuth': {
-		'clientID': 'XXXXXXXXXXXXXXXXXXXXXXXXX',
-		'clientSecret': 'XXXXXXXXXXXXXXXXXXXXXXXXX',
-		'callbackURL': 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+		'clientID': 'c63be8b7e41b496597cc6db47ebd55f3',
+		'clientSecret': '7b8101ca372347c791285f8a6e0bb451',
+		'callbackURL': 'http://localhost:8080/auth/instagram/callback'
 	}
+
 };
