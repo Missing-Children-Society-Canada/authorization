70e501a9a0198eacd05496c7a5f46c30d5259a9b
Jef King <jefkin@microsoft.com>
2017-05-24 21:03:47 -0700

    cleaner

--- config/auth.js
+++ config/auth.js
@@ -7,29 +7,21 @@ module.exports = {
 		'clientID': process.env.FACEBOOK_CONSUMER_KEY || '189849514869492', // your App ID 
 		'clientSecret': process.env.FACEBOOK_CONSUMER_SECRET || 'fce4a5d219289346239642928a1f7c79', // your App Secret 
 		'callbackURL': 'http://mcsc-authorization-dev.azurewebsites.net/auth/facebook/callback'
-		//'callbackURL': 'http://localhost:8080/auth/facebook/callback'
-		// 'callbackURL' : process.env.ROOT + '/auth/facebook/callback';
 	},
 
 	'twitterAuth': {
 		'consumerKey': process.env.TWITTER_CONSUMER_KEY || 'q2JfZwE8BTIHHNruzRqaWubEI',
 		'consumerSecret': process.env.TWITTER_CONSUMER_SECRET || 'FI4nM1BuayKsMBLZ2fA4zicaOkNUvjrhfRgiDYngVQVBtkXRyT',
 		'callbackURL': 'http://mcsc-authorization-dev.azurewebsites.net/auth/twitter/callback'
-		//'callbackURL': 'http://localhost:8080/auth/twitter/callback'
-		// 'callbackURL' : process.env.ROOT + '/auth/twitter/callback';
 	},
 
 	'instagramAuth': {
 		'clientID': process.env.INSTAGRAM_CONSUMER_KEY || 'c63be8b7e41b496597cc6db47ebd55f3',
 		'clientSecret': process.env.INSTAGRAM_CONSUMER_SECRET || '7b8101ca372347c791285f8a6e0bb451',
 		'callbackURL': 'http://mcsc-authorization-dev.azurewebsites.net/auth/instagram/callback'
-		//'callbackURL': 'http://localhost:8080/auth/instagram/callback'
-		// 'callbackURL' : process.env.ROOT + '/auth/instagram/callback';
 	},
 
 	'functions': {
-		//'callbackURL': 'https://mscs-cf-functions.azurewebsites.net/api/ig_subscriptions'
 		'callbackURL': 'https://mcsc-supporting-api.azurewebsites.net/api/instagram_subscriptions'
 	}
-
 };
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // config/database.js
 module.exports = {
-  'url': process.env.MONGO_DB || 'mongodb://missingchildrendata:agHqTdTA7xaIlSEnaVU63O0sFCnrSZuZHYcBLLoUgeyPCYC5qYSSFca7eMGbqRFmPaAPeaakJmMxYSy0OvxHog==@missingchildrendata.documents.azure.com:10250/user?ssl=true&sslverifycertificate=false'
+  'url': process.env.MONGO_DB
 };
