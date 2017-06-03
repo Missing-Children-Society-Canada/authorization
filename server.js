452190c80bf977dd4d3a7c7b3fc99d81ee19c681
aramkoukia <aramkoukia@gmail.com>
2017-06-02 21:03:29 -0700

    added app insights to the auth nodejs project

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 var configDB = require('./config/database.js');
+const appInsights = require("applicationinsights");
+
+appInsights.setup("<instrumentation_key>");
+appInsights
+    .setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .start();
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
